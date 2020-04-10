79632044199253327cefdebf13e9ed050609f7b4
Jesse Adametz <jesseadametz@gmail.com>
2020-04-10 13:17:37 -0700

    Frontend: initial new software page

---
+++ app/javascript/components/NewSoftware.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class NewSoftware extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      org: "",
+      full_name: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.stripHtmlEntities = this.stripHtmlEntities.bind(this);
+  }
+
+  stripHtmlEntities(str) {
+    return String(str)
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const url = "/api/v1/software/create";
+    const { name, org, full_name } = this.state;
+
+    if (name.length == 0 || org.length == 0 || full_name.length == 0)
+      return;
+    
+    const body = {
+      name,
+      org,
+      full_name: org + "/" + name
+    };
+
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.")
+      })
+      .then(response => this.props.history.push(`/software/${response.id}`))
+      .catch(error => console.log(error.message));
+  }
+
+  render() {
+    return (
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-sm-12 col-lg-6 offset-lg-3">
+            <h1 className="font-weight-normal mb-5">
+              Add new software.
+            </h1>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <label htmlFor="softwareName">Software Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="softwareName"
+                  className="form-control"
+                  required
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="gitHubOrg">GitHub Organization</label>
+                <input
+                  type="text"
+                  name="org"
+                  id="gitHubOrg"
+                  className="form-control"
+                  required
+                  onChange={this.onChange}
+                />
+              </div>
+              {/* <label htmlFor="instruction">Preparation Instructions</label>
+              <textarea
+                className="form-control"
+                id="instruction"
+                name="instruction"
+                rows="5"
+                required
+                onChange={this.onChange}
+              /> */}
+              <button type="submit" className="btn custom-button mt-3">
+                Create software
+              </button>
+              <Link to="/softwares" className="btn btn-link mt-3">
+                Back to software list
+              </Link>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewSoftware;
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Softwares from "../components/Softwares";
 import Software from "../components/Software";
+import NewSoftware from "../components/NewSoftware";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/software" exact component={Softwares} />
+      <Route path="/softwares" exact component={Softwares} />
       <Route path="/software/:id" exact component={Software} />
+      <Route path="/software" exact component={NewSoftware} />
     </Switch>
   </Router>
 );
