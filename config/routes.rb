3b76e56804dabda10d7e64ded16bd2ae419c1c11
Dan <dpramann@invoca.com>
2020-06-12 16:27:52 -0500

    Ensure format by default is json on api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get 'software/index'
       post 'software/create'
